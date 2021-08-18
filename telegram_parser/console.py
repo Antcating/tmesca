b836237c1a47a399ce8ee84796a95490e596ec3a
Antcating <dxtmvcqactxxdsokdf@protonmail.com>
2021-08-18 11:02:08 +0300

    console.py rewritten

--- telegram_parser/console.py
+++ telegram_parser/console.py
@@ -1,96 +1,157 @@
-import os, telebot
-from main import main    
+import os, telebot, configparser
+from main import main
+
+
+def update_config(config_element):
+    config_element.read('config.ini')
+    return config_element
+
 
 def telegram_channel_settings():
     try:
-        tg_token = open('.telegram_token', 'r').read()
-    except:
-        tg_token = input('Input Telegram bot token: ')
-        open('.telegram_token', 'w').write(tg_token)
-    try:
-        tg_address = open('.telegram_channel', 'r').read()
-    except:
-        tg_address = input('Input Telegram bot token: ')
-        open('.telegram_channel', 'w').write(tg_address)
+        config_element = configparser.ConfigParser()
+        config = update_config(config_element)
+        tg_token = config['TelegramBot']['token']
 
-def telegram_print_function(output):
-    tg_token = open('.telegram_token', 'r').read()
-    tg_address = open('.telegram_channel', 'r').read()
-    bot = telebot.TeleBot(tg_token)
-    bot.send_message(tg_address, output) 
-    
-    
+        bot = telebot.TeleBot(tg_token)
+        user_id = config['Admin']['id']
+        try:
+            bot.send_message(user_id, 'Test message')
+        except telebot.apihelper.ApiException as telebot_error:
+            if telebot_error.result.status_code == 404:
+                print('Telegram config incorrect. Aborting script...')
+                os.abort()
+
+        return bot, user_id
+    except KeyError:
+        print('Config does not exist. Aborting script...')
+        os.abort()
+
+
+def telegram_print_function(bot, user_id, output):
+    bot.send_message(user_id, output)
+
 
 def start_link():
     n_letters = input('How many letters in the link might be (at least 5): ')
-    start_point = 'a' + '1'*(int(n_letters)-1)
+    start_point = 'a' + '1' * (int(n_letters) - 1)
     open('.last_link', 'w').write(start_point)
 
 
-def console_start(print):
-    parser_type = input('''What type of content do you want to parse (input several numbers, if you want to parse any combination of the possible content): 
+def console_menu(print):
+    parser_config = {
+        'parser_type': None,
+        'bot_mode': None,
+        'work_mode': None,
+        'turbo_mode': None,
+        'output_source': None,
+        'output': None,
+        'print': None
+    }
+
+    def parsing_content_func(parser_config):
+        parser_type = input('''
+What type of content do you want to parse (input several numbers, if you want to parse any combination of content): 
  1 - All (Groups/Channel/Users
  2 - Channels
  3 - Groups
  4 - Users
  5 - Stickers
  6 - Bots
-Your choise: ''')[:2].lower()
-    if parser_type == '1' or parser_type == '2':
-        bot_mode = input('''Choose bot link types to parse: 1/2/12:
+Your choice: ''')[:2].lower()
+        if parser_type[0] not in ['1', '2', '3', '4', '5', '6']:
+            print('Incorrect input for the parsing content types. Try again.')
+            parsing_content_func(parser_config)
+
+        if parser_type == '1' or '6' in parser_type:
+            bot_mode = input('''
+Choose bot link types to parse: 1/2/12:
  1 - LINK_bot
  2 - LINKbot
  12 - LINK_bot && LINKbot
-Your choise: ''')
-    work_mode = input('''What type of parsing you want to use:
+Your choice: ''')
+            parser_config['bot_mode'] = bot_mode
+        parser_config['parser_type'] = parser_type
+        return parser_config
+
+    def work_mode_func(parser_config):
+        work_mode = input('''
+What type of parsing you want to use:
  1 - Linear parsing
  2 - Random parsing 
  3 - Mutation parsing 
-Your choise: ''')[0].lower()
-    turbo_mode = input('Turn on turbo mod(y/n): ')[0].lower()                   # work mode with/out delay
-    if turbo_mode == 'y':
-        turbo_mode = True
-        
-        
-    output_source_list = input('''What output source do you want: 
- 1 - Console
- 2 - Telegram
-Your choise: ''')[0].lower()    
-    if output_source_list == '1':
-        pass
-    elif output_source_list == '2':
-        telegram_channel_settings()
-        print = telegram_print_function    
+Your choice: ''')[0].lower()
+        if work_mode not in ['1', '2', '3']:
+            print('Incorrect input for the work mode. Try again.')
+            work_mode_func(parser_config)
+        parser_config['work_mode'] = work_mode
+        return parser_config
 
+    def turbo_mode_func(parser_config):
+        turbo_mode = input('Turn on turbo mod(y/n): ')[0].lower()  # work mode with/out delay
+        if turbo_mode in ['y', '1']:
+            parser_config['turbo_mode'] = True
+        elif turbo_mode in ['n', '0']:
+            parser_config['turbo_mode'] = False
+        else:
+            print('Incorrect input for the turbo mode. Try again.')
+            turbo_mode_func(parser_config)
+        return parser_config
 
-
-    output = input('''What type of output do you want: 
+    def output_func(parser_config):
+        output = input(
+'''What type of output do you want: 
  1 - All output (not False will be only the content, that was choosed to parse)
  2 - If something found
  3 - No output
 Your choise: ''')[0].lower()
-    fast_mode = input('Turn on only addresses write mode(y/n): ')[0].lower()
-    if fast_mode == 'y':
-        fast_mode = True
-    
-    window = False
+        if output in ['1', '2']:
+            parser_config['output'] = output
+            output_source = input(
+'''What output source do you want: 
+ 1 - Console
+ 2 - Telegram
+Your choice: ''')[0].lower()
+            if output_source == '1':
+                pass
+            elif output_source == '2':
+                bot, user_id = telegram_channel_settings()
+                print = telegram_print_function
+            return parser_config
+        elif output == '3':
+            parser_config['output'] = output
+            return parser_config
+        else:
+            print('Incorrect input for the output. Try again.')
+            output_func(parser_config)
+
+    parser_config = parsing_content_func(parser_config)
+    parser_config = work_mode_func(parser_config)
+    parser_config = turbo_mode_func(parser_config)
+    parser_config = output_func(parser_config)
+    # fast_mode = input('Turn on only addresses write mode(y/n): ')[0].lower()
+    # if fast_mode == 'y':
+    #     fast_mode = True
+
     mutated_initial_link = None
-    if work_mode == '1':
-        try:                                    # LINK Checking
+    if parser_config['work_mode'] == '1':
+        try:  # LINK Checking
             open('.last_link').read()
             change_start = input('Do you want to change number of letters in link(y/n): ')[0].lower()
             if change_start == 'y':
                 start_link()
-        except:
+        except FileNotFoundError:
             print('Initial setup!')
             start_link()
-            
-    if work_mode == '3':
+
+    elif parser_config['work_mode'] == '3':
         try:
             os.remove('mutated')
-        except:
+        except FileNotFoundError:
             pass
-        mutated_initial_link =  input('Input initial word to mutate (length of the word is greater than 5 letters): ').lower()
-    main(work_mode, parser_type, window, turbo_mode, fast_mode, output, print, mutated_initial_link, bot_mode)
-    
-console_start(print)
+        mutated_initial_link = input(
+            'Input initial word to mutate (length of the word is greater than 5 letters): ').lower()
+    main(parser_config)
+
+
+console_menu(print)
