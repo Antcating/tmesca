e836fa84be5be2c7df72062824307b4a73246af6
Dark Hole <dark.hole1@yandex.ru>
2021-08-19 15:52:47 +0300

    Remove old code

--- telegram_parser/link_generator.py
+++ telegram_parser/link_generator.py
@@ -6,55 +6,14 @@ ALPHABET_FIRST = string.ascii_lowercase
 ALPHABET_LAST = ALPHABET_FIRST + string.digits
 ALPHABET = ALPHABET_LAST + '_'
 
-def alphabets_generator():
-    return ALPHABET, ALPHABET_FIRST, ALPHABET_LAST
-
-
-def random_address_generator(alphabet, alphabet1, alphabet_last):
-    len_link = random.randint(5, 32)
-    link = random.choices(alphabet1)
-    link += random.choices(alphabet, k=len_link - 2)
-    link += random.choices(alphabet_last)
-    return ''.join(link)
-
-
 def random_addresses():
-    alphabet, alphabet_first, alphabet_last = alphabets_generator()
     while True:
         len_link = random.randint(5, 32)
-        link = random.choices(alphabet_first)
-        link += random.choices(alphabet, k=len_link - 2)
-        link += random.choices(alphabet_last)
+        link = random.choices(ALPHABET_FIRST)
+        link += random.choices(ALPHABET, k=len_link - 2)
+        link += random.choices(ALPHABET_LAST)
         yield ''.join(link)
 
-def last_link_read_linear_address(alphabet, alphabet1, alphabet_last):
-    start_point = open('.last_link').read()
-    linear_letter_link_ids_array = [
-        alphabet.index(letter) for letter in start_point
-    ]
-    return linear_letter_link_ids_array
-
-
-def linear_address_generator(alphabet, alphabet1, alphabet_last, linear_letter_link_ids_array, parser_config):
-    link = ''.join(alphabet[id] for id in linear_letter_link_ids_array)
-
-    linear_letter_link_ids_array[-1] += 1
-    if linear_letter_link_ids_array[-1] == len(alphabet_last):
-        linear_letter_link_ids_array[-1] = 0
-        linear_letter_link_ids_array[-2] += 1
-
-        i = len(linear_letter_link_ids_array) - 2
-        while i > 0:
-            if linear_letter_link_ids_array[i] < len(alphabet):
-                break
-            linear_letter_link_ids_array[i] = 0
-            linear_letter_link_ids_array[i-1] += 1
-            i -= 1
-        
-        if linear_letter_link_ids_array[0] == len(alphabet1):
-            print_func(parser_config, 'The end of this linear range. Exiting the program.')
-        
-    return link
 
 def linear_addresses(seed, first=True):
     if len(seed) == 1:
--- telegram_parser/main.py
+++ telegram_parser/main.py
@@ -28,9 +28,6 @@ def main(parser_config):
     fast_database_check()
     channel_db, group_db, user_db, stickers_db, bot0_db, bot1_db = telegram_parser_open()
     channel_fast_db, group_fast_db, user_fast_db, stickers_fast_db, bot0_fast_db, bot1_fast_db = fast_telegram_parser_open()
-    # alphabet, alphabet1, alphabet_last = alphabets_generator()
-    # if parser_config['work_mode'] == '1':
-    #     linear_letter_link_ids_array = last_link_read_linear_address(alphabet, alphabet1, alphabet_last)
     with open('.last_link') as f:
         seed = f.read()
 
