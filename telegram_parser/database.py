b07ceb3a26a6d789e2e1589347cf964e1ce67998
Dark Hole <dark.hole1@yandex.ru>
2021-08-21 12:26:22 +0300

    Added opening files

--- telegram_parser/database.py
+++ telegram_parser/database.py
@@ -1,8 +1,10 @@
 from database_check import database_check, fast_database_check
+from link_processor import telegram_parser_open, fast_telegram_parser_open
 
 class Database:
     def __init__(self, fast, parser_config):
         self.config = parser_config
+        self.fast = fast
         if fast:
             self.open_fast()
         else:
@@ -10,6 +12,13 @@ class Database:
     
     def open(self):
         database_check(self.config)
+        dbs = telegram_parser_open()
+        self._channel = dbs[0]
+        self._group = dbs[1]
+        self._user = dbs[2]
+        self._stickers = dbs[3]
+        self._bots = dbs[4]
+        # dbs[5] is actually ignored
     
     def open_fast(self):
         fast_database_check()
