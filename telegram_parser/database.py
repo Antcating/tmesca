19be2907047e4c713c273e548db5b2beaccee0a9
Dark Hole <dark.hole1@yandex.ru>
2021-08-21 12:34:21 +0300

    Added method for adding channels

--- telegram_parser/database.py
+++ telegram_parser/database.py
@@ -1,5 +1,7 @@
 from database_check import database_check, fast_database_check
 from link_processor import telegram_parser_open, fast_telegram_parser_open
+import csv
+
 
 class Database:
     def __init__(self, fast, parser_config):
@@ -9,23 +11,35 @@ class Database:
             self.open_fast()
         else:
             self.open()
-    
+
     def open(self):
         database_check(self.config)
         dbs = telegram_parser_open()
-        self._channel = dbs[0]
-        self._group = dbs[1]
-        self._user = dbs[2]
+        self._channels = dbs[0]
+        self._groups = dbs[1]
+        self._users = dbs[2]
         self._stickers = dbs[3]
         self._bots = dbs[4]
         # dbs[5] is actually ignored
-    
+
     def open_fast(self):
         fast_database_check()
         dbs = fast_telegram_parser_open()
-        self._channel = dbs[0]
-        self._group = dbs[1]
-        self._user = dbs[2]
+        self._channels = dbs[0]
+        self._groups = dbs[1]
+        self._users = dbs[2]
         self._stickers = dbs[3]
         self._bots = dbs[4]
         # dbs[5] is actually ignored
+
+    def add_channel(self, address, title, description, members):
+        if self.fast:
+            raise Exception('Attempt to add full data in fast db')
+        writer = csv.writer(self._channels)
+        writer.writerow([ address, title, description, members ])
+    
+    def add_channel_fast(self, address):
+        if not self.fast:
+            raise Exception('Attempt to add fast data in full db')
+        writer = csv.writer(self._channels)
+        writer.writerow([ address ])
