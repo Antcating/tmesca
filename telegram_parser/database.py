ad2fc83da0809e2edff53f7a065d1842f6e77b73
Dark Hole <dark.hole1@yandex.ru>
2021-08-21 12:20:23 +0300

    Added database creatiing

--- telegram_parser/database.py
+++ telegram_parser/database.py
@@ -1,12 +1,15 @@
+from database_check import database_check, fast_database_check
+
 class Database:
-    def __init__(self, fast):
+    def __init__(self, fast, parser_config):
+        self.config = parser_config
         if fast:
             self.open_fast()
         else:
             self.open()
     
     def open(self):
-        pass
+        database_check(self.config)
     
     def open_fast(self):
-        pass
+        fast_database_check()
