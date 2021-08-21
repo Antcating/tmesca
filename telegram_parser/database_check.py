b321a1ea2ec4b36ae8e1c7a90eb13ccfc423da2a
Dark Hole <dark.hole1@yandex.ru>
2021-08-21 11:37:14 +0300

    Formatting

--- telegram_parser/database_check.py
+++ telegram_parser/database_check.py
@@ -1,5 +1,8 @@
-import csv, os
+import csv
+import os
+from pathlib import Path
 from print_handler import print_func
+
 
 def database_check(parser_config):
     database_names_list = [
@@ -8,9 +11,10 @@ def database_check(parser_config):
         'user_telegram_parser.csv',
         'stickers_telegram_parser.csv',
         'bots_telegram_parser.csv',
-        ]
+    ]
     for database_name in database_names_list:
-        try:                                                                    # checking existing of the telegram_parser.csv file
+        # checking existing of the telegram_parser.csv file
+        try:
             open('output/' + database_name, 'r').close()
         except FileNotFoundError:
             try:
@@ -18,16 +22,19 @@ def database_check(parser_config):
             except FileExistsError:
                 pass
             open('output/' + database_name, 'a+')
-            with open('output/' + database_name,'a',newline='') as f:
-                csv_writer=csv.writer(f)
+            with open('output/' + database_name, 'a', newline='') as f:
+                csv_writer = csv.writer(f)
                 if 'group' in database_name or 'channel' in database_name:
-                    csv_writer.writerow(['adress', 'title', 'description', 'members'])
+                    csv_writer.writerow(
+                        ['adress', 'title', 'description', 'members'])
                 elif 'user' in database_name or 'bots' in database_name:
                     csv_writer.writerow(['adress', 'title', 'description'])
                 else:
                     csv_writer.writerow(['adress', 'name'])
-            print_func(parser_config, 'Init Info:' + database_name + ' was created in the script folder')
-        
+            print_func(parser_config, 'Init Info:' + database_name +
+                       ' was created in the script folder')
+
+
 def fast_database_check():
     database_names_list = [
         'channel_telegram_parser_fast.csv',
@@ -35,9 +42,10 @@ def fast_database_check():
         'user_telegram_parser_fast.csv',
         'stickers_telegram_parser_fast.csv',
         'bots_telegram_parser_fast.csv',
-        ]
+    ]
     for database_name in database_names_list:
-        try:                                                                    # checking existing of the telegram_parser.csv file
+        # checking existing of the telegram_parser.csv file
+        try:
             open('output/' + database_name, 'r').close()
         except FileNotFoundError:
             try:
@@ -45,6 +53,6 @@ def fast_database_check():
             except FileExistsError:
                 pass
             open('output/' + database_name, 'a+')
-            with open('output/' + database_name,'a',newline='') as f:
-                csv_writer=csv.writer(f)
+            with open('output/' + database_name, 'a', newline='') as f:
+                csv_writer = csv.writer(f)
                 csv_writer.writerow(['adress'])
