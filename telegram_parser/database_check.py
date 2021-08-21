683a004c2c818068fb6e6c05b5ba5a5e41999d8e
Dark Hole <dark.hole1@yandex.ru>
2021-08-21 12:01:08 +0300

    Rewrited fast db check to pathlib

--- telegram_parser/database_check.py
+++ telegram_parser/database_check.py
@@ -5,7 +5,7 @@ from print_handler import print_func
 
 
 def database_check(parser_config):
-    database_names_list = [
+    database_list = [
         {
             'name': 'channels.csv',
             'fields': ['address', 'title', 'description', 'members']
@@ -27,7 +27,7 @@ def database_check(parser_config):
     if not output.exists():
         output.mkdir()
 
-    for database in database_names_list:
+    for database in database_list:
         # checking existing of the telegram_parser.csv file
         path = output.joinpath(database['name'])
         if not path.exists():
@@ -46,16 +46,14 @@ def fast_database_check():
         'stickers_telegram_parser_fast.csv',
         'bots_telegram_parser_fast.csv',
     ]
+    output = Path('output')
+    if not output.exists():
+        output.mkdir()
+
     for database_name in database_names_list:
         # checking existing of the telegram_parser.csv file
-        try:
-            open('output/' + database_name, 'r').close()
-        except FileNotFoundError:
-            try:
-                os.mkdir('output')
-            except FileExistsError:
-                pass
-            open('output/' + database_name, 'a+')
-            with open('output/' + database_name, 'a', newline='') as f:
+        path = output.joinpath(database_name)
+        if not path.exists():
+            with path.open('w', newline='') as f:
                 csv_writer = csv.writer(f)
-                csv_writer.writerow(['adress'])
+                csv_writer.writerow(['address'])
