fc69f7219e2de714e91253dddb9e3689e686cf52
Dark Hole <dark.hole1@yandex.ru>
2021-08-22 20:50:10 +0300

    Add link length

--- tmesca/config.py
+++ tmesca/config.py
@@ -1,7 +1,8 @@
 from pathlib import Path
 
 import yaml
-from questionary import Choice, select, confirm
+from questionary import Choice, select, confirm, text
+import re
 
 
 class Config:
@@ -24,21 +25,21 @@ class Config:
 
         if prompt:
             self.generator_prompt()
-        
-        # self.restore_session()
+
+        # self.init_session()
 
     def generator_prompt(self):
         gen = self.generator
         que = QUESTIONS['generator']
         if 'type' not in gen:
             gen['type'] = que['type'].ask()
-        
+
         if gen['type'] == 'linear':
             last_link = Path('.last_link')
             if last_link.is_file() and 'restore_sessions' not in gen:
                 gen['restore_sessions'] = que['restore_sessions'].ask()
-            if not last_link.is_file() or not gen['restore_sessions']:
-                pass
+            if (not last_link.is_file() or not gen['restore_sessions']) and 'link_length' not in gen:
+                gen['link_length'] = int(que['link_length'].ask())
 
 
 QUESTIONS = {
@@ -53,6 +54,10 @@ QUESTIONS = {
         'restore_sessions': confirm(
             message='Continue old session?',
             default=True
+        ),
+        'link_length': text(
+            message='Enter link length:',
+            validate=lambda x: re.match(r'^\d+$', x) is not None
         )
     }
 }
