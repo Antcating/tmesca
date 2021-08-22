0bad669c9637155aa3d6dfe6892796fdc7598702
Dark Hole <dark.hole1@yandex.ru>
2021-08-22 19:33:00 +0300

    Added load config

--- tmesca/config.py
+++ tmesca/config.py
@@ -1,3 +1,19 @@
+import yaml
+from pathlib import Path
+
+
 class Config:
-    def __init__(self, filename='tmesca.yml'):
-        pass
+    def __init__(self, prompt=False, filename='tmesca.yml'):
+        config = {
+            'generator': {},
+            'parser': {},
+            'output': {}
+        }
+        path = Path(filename)
+        if path.is_file():
+            with path.open('r') as f:
+                config.update(yaml.safe_load(f))
+        
+        self.generator = config['generator']
+        self.parser = config['parser']
+        self.output = config['output']
