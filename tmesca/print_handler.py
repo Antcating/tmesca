8e9aa9a3016b563eb7cad59c5845ab9c5c3a81d2
Dark Hole <dark.hole1@yandex.ru>
2021-08-26 16:56:07 +0300

    Remove old code

--- tmesca/print_handler.py
+++
@@ -1,6 +0,0 @@
-def print_func(parser_config, output):
-    if parser_config['output_source'] == '1':
-        print(output)
-    else:
-        parser_config['bot'].send_message(parser_config['user_id'],
-                                          output)
